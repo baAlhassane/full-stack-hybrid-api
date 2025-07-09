ec024c19703a2aaa90f297553650c457312a6015
Alhassane Ba <alhassanebai36@gmail.com>
2025-07-09 13:46:00 +0200

    change port spring boot, url dans angular dans evv.prod

--- hybrid-api-front/src/environments/environment.prod.ts
+++ hybrid-api-front/src/environments/environment.prod.ts
@@ -1,5 +1,12 @@
-export const environment = {
+    export const environment = {
+      production: true, // Cette ligne est correcte
+      // Modifiez API_URL pour pointer vers l'IP de votre WSL et le nouveau port du backend (8081)
+      API_URL : "http://172.31.92.36:8081/api/hybrid-api"
+    };
+    
 
-production: true, // Ajoutez cette ligne
- API_URL : "http://localhost:8080/api/hybrid-api"
-};
+// export const environment = {
+
+// production: true, // Ajoutez cette ligne
+//  API_URL : "http://localhost:8080/api/hybrid-api"
+// };
