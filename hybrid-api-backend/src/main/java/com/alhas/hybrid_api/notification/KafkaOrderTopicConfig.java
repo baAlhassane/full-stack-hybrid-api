665d34fddb68fbdcd6f0ed1d5848b165d0130368
baAlhassane <alhassanebai36@gmail.com>
2025-09-15 11:51:11 +0200

    feat: Save local changes before  update

--- hybrid-api-backend/src/main/java/com/alhas/hybrid_api/notification/KafkaOrderTopicConfig.java
+++ hybrid-api-backend/src/main/java/com/alhas/hybrid_api/notification/KafkaOrderTopicConfig.java
@@ -1,10 +1,8 @@
 package com.alhas.hybrid_api.notification;
 
-<<<<<<< Updated upstream
 import org.springframework.context.annotation.Bean;
 
-=======
->>>>>>> Stashed changes
+
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,11 +14,7 @@ public class KafkaOrderTopicConfig {
     @Bean
     public NewTopic ordersTopic() {
         return TopicBuilder
-<<<<<<< Updated upstream
-                .name("order-topic")
-=======
                 .name("user-registrations")
->>>>>>> Stashed changes
                 .build();
     }
 }
--- hybrid-api-backend/src/main/java/com/alhas/hybrid_api/notification/NotificationProducer.java
+++ hybrid-api-backend/src/main/java/com/alhas/hybrid_api/notification/NotificationProducer.java
@@ -1,28 +1,14 @@
 package com.alhas.hybrid_api.notification;
 
 
-<<<<<<< Updated upstream
-
-
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.stereotype.Service;
-
-
-
-=======
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
-
->>>>>>> Stashed changes
 @Service
 @Slf4j
 public class NotificationProducer {
-<<<<<<< Updated upstream
 
-=======
->>>>>>> Stashed changes
     private final KafkaTemplate<String, UserEvent> kafkaTemplate;
 
     public NotificationProducer(KafkaTemplate<String, UserEvent> kafkaTemplate) {
@@ -35,6 +21,5 @@ public class NotificationProducer {
     }
 
 }
-
 
 
