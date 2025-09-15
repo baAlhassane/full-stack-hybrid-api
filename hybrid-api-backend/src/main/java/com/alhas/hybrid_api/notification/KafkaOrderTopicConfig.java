4d3278031f2bc80c7cc48c6b367365ad373b3e25
baAlhassane <alhassanebai36@gmail.com>
2025-09-15 11:37:45 +0200

    feat: Sauvegarde des modifications locales avant d'appliquer le stash

--- hybrid-api-backend/src/main/java/com/alhas/hybrid_api/notification/KafkaOrderTopicConfig.java
+++ hybrid-api-backend/src/main/java/com/alhas/hybrid_api/notification/KafkaOrderTopicConfig.java
@@ -1,4 +1,19 @@
 package com.alhas.hybrid_api.notification;
 
-public class KafkTopic {
+import org.springframework.context.annotation.Bean;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaOrderTopicConfig {
+
+    @Bean
+    public NewTopic ordersTopic() {
+        return TopicBuilder
+                .name("order-topic")
+                .build();
+    }
 }
--- hybrid-api-backend/src/main/java/com/alhas/hybrid_api/notification/NotificationProducer.java
+++ hybrid-api-backend/src/main/java/com/alhas/hybrid_api/notification/NotificationProducer.java
@@ -1,24 +1,16 @@
 package com.alhas.hybrid_api.notification;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Entity
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
+
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+
+
 @Service
 public class NotificationProducer {
-    @Id
-    @GeneratedValue
-    private long id;
+
     private final KafkaTemplate<String, UserEvent> kafkaTemplate;
 
     public NotificationProducer(KafkaTemplate<String, UserEvent> kafkaTemplate) {
